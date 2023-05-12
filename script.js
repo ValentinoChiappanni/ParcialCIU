396afa9a2dea39d488b453b0bc19cff9ec3aa037
ValentinoChiappanni <valentino.chiappanni077@gmail.com>
2023-05-11 22:10:52 -0300

    sol y luna

--- script.js
+++ script.js
@@ -1,4 +1,5 @@
 const darkModeButton = document.getElementById('darkModeButton');
+const img = document.getElementById('dark');
 const body = document.body;
 var estaOscuro = true
 function toggleDarkMode() {
@@ -7,13 +8,13 @@ function toggleDarkMode() {
         logoImage.src = "./asset/logoB.png";
         body.classList.toggle('dark-mode');
         body.classList.remove('fondo');
-
+        img.src = "./asset/sol.png";
         estaOscuro = !estaOscuro
     } else {
         logoImage.src = "./asset/logoN.png";
         body.classList.remove('dark-mode');
         body.classList.toggle('fondo');
-
+        img.src = "./asset/luna.png";
         estaOscuro = !estaOscuro
     }
 }
