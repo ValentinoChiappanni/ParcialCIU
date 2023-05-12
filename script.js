619c18683ded343bc6f397f110f564a7d30e01f7
ValentinoChiappanni <valentino.chiappanni077@gmail.com>
2023-05-11 21:34:15 -0300

    fondo

--- script.js
+++ script.js
@@ -4,16 +4,16 @@ var estaOscuro = true
 function toggleDarkMode() {
     var logoImage = document.getElementById("logo");
     if (estaOscuro) {
-        console.log('Negro')
         logoImage.src = "./asset/logoB.png";
         body.classList.toggle('dark-mode');
         body.classList.remove('fondo');
+
         estaOscuro = !estaOscuro
     } else {
-        console.log('Blanco')
         logoImage.src = "./asset/logoN.png";
         body.classList.remove('dark-mode');
         body.classList.toggle('fondo');
+
         estaOscuro = !estaOscuro
     }
 }
